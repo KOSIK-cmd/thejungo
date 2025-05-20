e57b3a87b2c3c636b1d94f99cab339aa4f5c6878
KOSIK <kosik@KOSU.KOSU>
2025-05-20 22:31:56 +0200

    problemy 1 świata

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,12 @@
 from django.urls import include, path
 from django.contrib import admin
+from watson.urls import app_name
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("polls/", include("polls.urls")),
-    path("admin/", admin.site.urls),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
